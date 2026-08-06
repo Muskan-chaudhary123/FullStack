d7b9a645f7886d2701ad0c7d806b668f7c3acb88
Muskan-chaudhary123 <chaudharymuskan713@gmail.com>
2026-08-06 18:34:26 +0530

    Added commit

---
+++ Day3/globalDir.js
@@ -0,0 +1,10 @@
+console.log(__dirname)
+
+console.log(__filename)
+
+// Using Process
+console.log(process.cwd)
+console.log(process.version)
+console.log(process.pid)
+console.log(process.platform)
+console.log(process.env.USER)
---
+++ Day3/index.js
@@ -0,0 +1,36 @@
+// Using Lodash package
+
+// const _ = require('lodash');
+
+// const arr = [ 1, 2 , 3 , 4, 5, 6, 7, 8, 9, 10];
+
+// const chunkedArr = _.chunk(arr,3)
+
+// console.log(chunkedArr)
+
+
+// Using Chalk
+
+// const chalk = require('chalk')
+
+// console.log(chalk.blue('Hello'))
+
+// console.log(chalk.red("World"))
+
+// Using Axios
+
+// const axios = require('axios')
+
+// const url = 'https://jsonplaceholder.typicode.com/posts'
+
+// axios.get(url).then(response =>{
+//     console.log(response.data)
+// }).catch(error =>{
+//     console.log(error)
+// })
+
+// Using dayjs
+const dayjs = require('dayjs')
+
+console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+
